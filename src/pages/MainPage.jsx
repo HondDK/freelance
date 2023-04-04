ccbd239f48ecb19f7ced324c15283000efbd8b7a
HondDK <Honddk99@gmail.com>
2023-04-04 22:29:18 +0600

    sd

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const MainPage = () => {
+	return (
+		<>
+			<header>
+				<h1>FREELANCE</h1>{" "}
+				<div>
+					<button>Регистрация</button> <button>Вход</button>{" "}
+				</div>
+			</header>
+			<main>
+				<aside>
+					<div>
+						<p>
+							Разработчики, дизайнеры и другие профессионалы ИТ-рынка готовы
+							выполнить вашу задачу сейчас.
+						</p>
+						<button>Найти специалиста</button>
+					</div>
+					<div className="container">
+						Помогаем работать эффективнее.
+						<br /> Мы точно знаем, что фриланс — это больше, чем разовые заказы
+						или поиск более низкой ставки. <br />
+						Фриланс — это удобный, масштабируемый инструмент для компаний. А для
+						специалистов — возможность
+					</div>
+				</aside>
+				<article>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+					<div className="container">
+						<p>Разработка</p> <p>Сайты “Под ключ”, Бэкенд, Фронтенд и другое</p>{" "}
+						<button>Посмотреть</button>
+					</div>
+				</article>
+			</main>
+			<footer></footer>
+		</>
+	);
+};
+
+export default MainPage;
